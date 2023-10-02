ea866c2ef3f4e39308bcb63ffd4d9258ce2506b5
Dhruv <dhruvm2509@gmail.com>
2023-10-02 16:28:36 -0400

    added MinMaxCalculation.java

---
+++ src/Views/MinMaxCalculation.java
@@ -0,0 +1,23 @@
+public class MinMaxCalculation {
+
+  public static double min(double ... numbers) {
+    double min = numbers[0];
+    for (double num : numbers) {
+      if (num < min) {
+        min = num;
+      }
+    }
+    return min;
+  }
+
+  public static double max(double ... numbers) {
+    double max = numbers[0];
+    for (double num : numbers) {
+      if (num > max) {
+        max = num;
+      }
+    }
+    return max;
+  }
+
+}
